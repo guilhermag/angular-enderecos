b037aae9e76d19afa89aa3236efb4dc86a865fd4
Guilherme Gabriel <guilhermag@gmail.com>
2023-12-27 08:46:40 -0300

    finalizacao unit tests

--- src/app/views/endereco-form/endereco-form.component.spec.ts
+++ src/app/views/endereco-form/endereco-form.component.spec.ts
@@ -12,7 +12,6 @@ import { cepMock, enderecoMock, estadosMock } from 'src/app/shared/mocks/cep';
 import { of } from 'rxjs';
 import { deepClone } from 'src/app/shared/utils/utils';
 import { EnderecoService } from 'src/app/shared/services/endereco.service';
-import { EnderecoInfo } from 'src/app/shared/model/endereco';
 import { MatSelectChange } from '@angular/material/select';
 
 describe('EnderecoFormComponent', () => {
@@ -58,63 +57,76 @@ describe('EnderecoFormComponent', () => {
     component.form.get('cep').setValue('86709240');
     fixture.detectChanges();
     tick(1000);
-    expect(component.cepMensagem).toBe('Procurando CEP');
+    expect(component.cepMensagem).toEqual('Procurando CEP');
     tick(2000);
 
-    expect(component.cepMensagem).toBe('');
-    expect(component.form.get('logradouro').value).toBe(cepMock.logradouro);
-    expect(component.form.get('bairro').value).toBe(cepMock.bairro);
-    expect(component.form.get('localidade').value).toBe(cepMock.localidade);
-    expect(component.form.get('uf').value).toBe(cepMock.uf);
+    expect(component.cepMensagem).toEqual('');
+    expect(component.form.get('logradouro').value).toEqual(cepMock.logradouro);
+    expect(component.form.get('bairro').value).toEqual(cepMock.bairro);
+    expect(component.form.get('localidade').value).toEqual(cepMock.localidade);
+    expect(component.form.get('uf').value).toEqual(cepMock.uf);
   }));
 
-  it('deve popular o formulario com base no cep digitado, caso o cep não exista', fakeAsync(() => {
+  it('deve popular o formulario com valores em branco, caso o cep não exista', fakeAsync(() => {
     cepServiceMock.getCep.and.returnValue(of(null));
     component.form.get('cep').setValue('86709240');
     fixture.detectChanges();
     tick(2000);
-    expect(component.cepMensagem).toBe('CEP não encontrado');
+    expect(component.cepMensagem).toEqual('CEP não encontrado');
     tick(1000);
 
-    expect(component.cepMensagem).toBe('');
-    expect(component.form.get('logradouro').value).toBe('');
-    expect(component.form.get('bairro').value).toBe('');
-    expect(component.form.get('localidade').value).toBe('');
-    expect(component.form.get('uf').value).toBe('');
+    expect(component.cepMensagem).toEqual('');
+    expect(component.form.get('logradouro').value).toEqual('');
+    expect(component.form.get('bairro').value).toEqual('');
+    expect(component.form.get('localidade').value).toEqual('');
+    expect(component.form.get('uf').value).toEqual('');
   }));
 
-  it('deve enviar o formulario, caso seja válido', () => {
-    const info = deepClone(enderecoMock);
-    delete info.id;
-    component.form.get('cep').setValue(info.cep);
-    component.form.get('logradouro').setValue(info.logradouro);
-    component.form.get('numero').setValue(info.numero);
-    component.form.get('complemento').setValue(info.complemento);
-    component.form.get('bairro').setValue(info.bairro);
-    component.form.get('localidade').setValue(info.localidade);
-    component.form.get('uf').setValue(info.uf);
-    component.submeterForm();
-    expect(enderecoServiceMock.salvarEndereco).toHaveBeenCalledWith(info);
-    component.modo = 'edicao';
-    component.submeterForm();
-    expect(enderecoServiceMock.salvarEndereco).toHaveBeenCalledWith(info);
-  });
+  it('não deve popular o formulario, caso o cep seja nulo', fakeAsync(() => {
+    cepServiceMock.getCep.and.returnValue(of(null));
+    component.form.get('cep').setValue(null);
+    fixture.detectChanges();
+    tick(1000);
+    expect(component.cepMensagem).toEqual('');
+    tick(2000);
 
-  it('deve enviar o formulario, caso seja válido no modo edicao', () => {
-    const info = deepClone(enderecoMock);
-    info.horarioPesquisa = '';
-    component.enderecoEditado = info;
-    component.modo = 'edicao';
+    expect(component.form.get('logradouro').value).toEqual('');
+    expect(component.form.get('bairro').value).toEqual('');
+    expect(component.form.get('localidade').value).toEqual('');
+    expect(component.form.get('uf').value).toEqual('');
+  }));
 
-    component.form.get('cep').setValue(info.cep);
-    component.form.get('logradouro').setValue(info.logradouro);
-    component.form.get('numero').setValue(info.numero);
-    component.form.get('complemento').setValue(info.complemento);
-    component.form.get('bairro').setValue(info.bairro);
-    component.form.get('localidade').setValue(info.localidade);
-    component.form.get('uf').setValue(info.uf);
-    component.submeterForm();
-    expect(enderecoServiceMock.editarEndereco).toHaveBeenCalledWith(info);
+  describe('enviar formulario', () => {
+    beforeEach(() => {
+      const info = deepClone(enderecoMock);
+      component.enderecoEditado = info;
+      component.form.get('cep').setValue(info.cep);
+      component.form.get('logradouro').setValue(info.logradouro);
+      component.form.get('numero').setValue(info.numero);
+      component.form.get('complemento').setValue(info.complemento);
+      component.form.get('bairro').setValue(info.bairro);
+      component.form.get('localidade').setValue(info.localidade);
+      component.form.get('uf').setValue(info.uf);
+      enderecoServiceMock.salvarEndereco.calls.reset();
+      enderecoServiceMock.editarEndereco.calls.reset();
+    });
+
+    it('salvar formulario, caso seja válido', () => {
+      const info = deepClone(enderecoMock);
+      delete info.id;
+      delete info.horarioPesquisa;
+      component.submeterForm();
+      expect(enderecoServiceMock.salvarEndereco).toHaveBeenCalledWith(info);
+    });
+
+    it('editar formulario, caso seja válido', () => {
+      const info = deepClone(enderecoMock);
+      info.horarioPesquisa = '';
+      component.enderecoEditado = info;
+      component.modo = 'edicao';
+      component.submeterForm();
+      expect(enderecoServiceMock.editarEndereco).toHaveBeenCalledWith(info);
+    });
   });
 
   it('deve mudar as propriedades do componente ao clicar em editar endereco', () => {
@@ -129,5 +141,30 @@ describe('EnderecoFormComponent', () => {
     const evento = { value: estadosMock[0].sigla } as MatSelectChange;
     component.selecionarEstado(evento);
     expect(component.estadoSelecionado).toEqual(estadosMock[0].sigla);
+  });
+
+  it('deve limpar o limpar o formulário', () => {
+    component.limparFormulario();
+    expect(component.modo).toEqual('criacao');
+    expect(component.botaoSubmeter).toEqual('cadastrar');
+  });
+
+  it('deve atualizar a lista de enderecos', () => {
+    const info = deepClone(enderecoMock);
+    component['atualizarListaEnderecos'](info);
+    expect(enderecoServiceMock.salvarEndereco).toHaveBeenCalledWith(
+      info,
+      'enderecosPesquisados'
+    );
+  });
+
+  it('deve popular o formulario', () => {
+    const info = deepClone(enderecoMock);
+    component['popularFormulario'](info);
+    expect(component.form.get('cep').value).toEqual(info.cep);
+    expect(component.form.get('logradouro').value).toEqual(info.logradouro);
+    expect(component.form.get('bairro').value).toEqual(info.bairro);
+    expect(component.form.get('localidade').value).toEqual(info.localidade);
+    expect(component.form.get('uf').value).toEqual(info.uf);
   });
 });
--- src/app/views/tabela-pesquisas/tabela-pesquisas.component.spec.ts
+++ src/app/views/tabela-pesquisas/tabela-pesquisas.component.spec.ts
@@ -3,15 +3,36 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TabelaPesquisasComponent } from './tabela-pesquisas.component';
 import { MaterialModule } from 'src/app/material/material.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { EnderecoService } from 'src/app/shared/services/endereco.service';
+import { MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { EnderecoInfo } from 'src/app/shared/model/endereco';
+import { deepClone } from 'src/app/shared/utils/utils';
+import { enderecoMock } from 'src/app/shared/mocks/cep';
+import { ModalComponent } from 'src/app/shared/components/modal/modal.component';
 
 describe('TabelaPesquisasComponent', () => {
   let component: TabelaPesquisasComponent;
   let fixture: ComponentFixture<TabelaPesquisasComponent>;
+  const info = deepClone(enderecoMock);
+
+  let dialogMock: jasmine.SpyObj<MatDialog> = jasmine.createSpyObj({
+    open: { afterClosed: () => of(true) },
+  });
+
+  let enderecoServiceMock: jasmine.SpyObj<EnderecoService> =
+    jasmine.createSpyObj({
+      excluirEndereco: null,
+    });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TabelaPesquisasComponent],
       imports: [MaterialModule, HttpClientTestingModule],
+      providers: [
+        { provide: MatDialog, useValue: dialogMock },
+        { provide: EnderecoService, useValue: enderecoServiceMock },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TabelaPesquisasComponent);
@@ -21,5 +42,17 @@ describe('TabelaPesquisasComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve emitir o objeto endereco quando o metodo editarEndereco e chamado', () => {
+    spyOn(component.editarEndereco, 'emit');
+    component.editar(info);
+    expect(component.editarEndereco.emit).toHaveBeenCalledWith(info);
+  });
+
+  it('deve chamar o metodo de excluir endereco quando o usuario confirmar a operacao', () => {
+    component.excluir(info);
+    expect(dialogMock.open).toHaveBeenCalledWith(ModalComponent);
+    expect(enderecoServiceMock.excluirEndereco).toHaveBeenCalledWith(info);
   });
 });
